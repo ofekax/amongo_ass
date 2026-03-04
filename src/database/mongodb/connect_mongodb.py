c362227fc8515ac413ad8a75dd1a9885e2d6b8a1
alonax <46304073+alonax@users.noreply.github.com>
2026-03-04 20:28:00 +0200

    Adding a deployments db,and  successfully connection to it

--- src/database/mongodb/connect_mongodb.py
+++ src/database/mongodb/connect_mongodb.py
@@ -1,12 +1,11 @@
 from pymongo import MongoClient
 
-
-uri = "mongodb://nraboy:password1234@localhost:27017/"
+uri = "mongodb://nraboy:password1234@localhost:27017"
 client = MongoClient(uri)
 try:
 
-    client.admin.command("ping")
-    print("Connected successfully")
+    client.deployments.command("ping")
+    print("Connected successfully to", client.get_database('deployments'))
     client.close()
 
 except Exception as e:
