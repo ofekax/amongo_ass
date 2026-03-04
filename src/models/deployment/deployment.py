fbf0400335234b3a3d47221e864e2fd67514a4a6
alonax <46304073+alonax@users.noreply.github.com>
2026-03-05 00:22:00 +0200

    trying to connect to postgres and padmin - NOT WORKING

--- src/models/deployment/deployment.py
+++ src/models/deployment/deployment.py
@@ -6,11 +6,10 @@ from conf import db
 from src.models.deployment.check_deployment import check_username
 
 
-
-
 class Deployment:
     def __init__(self, username: str):
         self.id: str = str(uuid.uuid4())
+        # Is the collection name?
         self.db_name: str = username + db.name
         self.status: str = 'CREATED'
         self.username = username
