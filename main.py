3041d1739e63888404d5c3a8a86a9deba2313a31
alonax <46304073+alonax@users.noreply.github.com>
2026-03-05 20:30:13 +0200

    deleted old deplyment class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # This is a sample Python script.
 from src.database.postgres.connect_to_postgres_db import db_engine
 from src.database.postgres.postgressql_and_padmin_settings import compose_file_setting
-from src.models.deployment.deployment_notgood import Deployment
 
 
 # Press Shift+F10 to execute it or replace it with your code.
